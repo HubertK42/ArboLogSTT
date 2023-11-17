918e1a3585f69c8207d177a1f56e80915913c48d
HubertK42 <rk@sciresolve.de>
2023-11-17 20:45:06 +0100

    ref: try to get the sound running

--- src/main.cpp
+++ src/main.cpp
@@ -24,17 +24,20 @@ int main(int argc, char *argv[])
 	setlocale(LC_ALL, "");
 	std::setlocale(LC_ALL, "C");
 	char filepath[245];
-	sprintf(filepath, "%s", "./test.flac");
+	//sprintf(filepath, "%s", "./test.flac");
+	sprintf(filepath, "%s", "./test2.wav");
 //---------------------------------------------------------		
 //-------------------- scan start params
-	std::cout << "TOLL!" << std::endl;
+	
 	audiohelper AH;
-	AH.init_recIntoFile(filepath, ma_encoding_format_flac, ma_device_type_capture, 16000, 1, ma_format_s16);
+	//AH.init_recIntoFile(filepath, ma_encoding_format_flac, ma_device_type_capture, 16000, 1, ma_format_s16);
+	AH.init_recIntoFile(filepath, ma_encoding_format_wav, ma_device_type_capture, 16000, 1, ma_format_s16);
 
-
+	std::cout << "start rec!" << std::endl;
 	AH.start_rec();						//!< capture sound from e.g. micro 
-		sleep(1);
+		sleep(3);
 	AH.stop();
+	std::cout << "start stopped!" << std::endl;
 
 	//AH.start_play(void *pBbuffer);		//!< play sound from pBuffer
 
